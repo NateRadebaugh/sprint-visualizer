c134ecdb0d10206956ccdc8185f968e4ac565e22
Nate Radebaugh <nradebaugh@bdo.com>
2023-01-28 22:11:55 -0600

    Avoid shared or nested layout for content

--- src/app/(index)/layout.tsx
+++ src/app/(index)/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import "../styles/global.scss";
+import "../../styles/global.scss";
 
 export interface RootLayoutProps {
   children: React.ReactNode;
---
+++ src/app/content/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "../../styles/global.scss";
+
+export interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
